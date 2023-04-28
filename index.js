47a14c581b0f08c96e486a074cd2fff09d8de1a6
Nyesh Dotson <dotsonnyesh@gmail.com>
2023-04-28 16:22:27 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
-// Write your code here
+const num1 = 31 
+const num2 = 2
+const multiply = 31 * 2
+const random = 1
+Math.floor(Math.random() * 10) + 1
+const num3 = 36
+const num4 = 8
+const mod = 36 % 8
+const max = (1,2,3,4,20)
+Math.max(1,2,3,4,20)
